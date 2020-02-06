72823d51e2a1b07aba747553f118fb51a8683eea
Corey Sutter <sutterco@mail.gvsu.edu>
2020-02-05 21:38:59 -0500

    Testing git commits

--- src/HotelManagement/Main.java
+++ src/HotelManagement/Main.java
@@ -3,6 +3,7 @@ package HotelManagement;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Corey Sutter");
+        System.out.println("Corey Sutter");
+        System.out.println("Testing Git");
     }
 }
