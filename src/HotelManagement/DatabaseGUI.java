de2730987842cd902106e55374992359809eec84
jkippy <kaipjason@gmail.com>
2020-02-17 20:35:20 -0500

    Made intital javafx changes.

---
+++ src/HotelManagement/DatabaseGUI.java
@@ -0,0 +1,15 @@
+package HotelManagement;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DatabaseGUI extends Application {
+
+    public static void main(String[]args){
+        launch(args);
+    }
+
+    @Override public void start(Stage primaryStage) throws Exception{
+
+    }
+}
