a7f95a5401a1dc47d58a034fd20d38a43166a839
jkippy <kaipjason@gmail.com>
2020-02-17 20:53:36 -0500

    more javafx stuff.

--- src/HotelManagement/DatabaseGUI.java
+++ src/HotelManagement/DatabaseGUI.java
@@ -1,15 +1,29 @@
 package HotelManagement;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class DatabaseGUI extends Application {
+
+    Button button;
 
     public static void main(String[]args){
         launch(args);
     }
 
     @Override public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Hotel Management System");
+        button = new Button();
+        button.setText("Test Button");
 
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
