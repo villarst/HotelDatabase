942852465754a4eb2aab3257d57ef30830515361
Steven Villarreal <villarst@mail.gvsu.edu>
2020-02-19 17:34:02 -0500

    I am commiting the files needed for GUI

---
+++ src/HotelManagement/Controller.java
@@ -0,0 +1,14 @@
+package HotelManagement;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void pressButton(ActionEvent event){
+        System.out.println("Hello World");
+    }
+
+    public void exitButton(ActionEvent event){
+        System.exit(0);
+    }
+}
