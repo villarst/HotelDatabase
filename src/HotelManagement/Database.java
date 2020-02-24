3a0dee5db66948630da7293c50395dc107f1d299
Steven Villarreal <villarst@mail.gvsu.edu>
2020-02-24 11:58:33 -0500

    Changed the assignRoom method to work correctly

--- src/HotelManagement/Database.java
+++ src/HotelManagement/Database.java
@@ -10,12 +10,22 @@ public class Database{
     private ArrayList <User> SecondaryDb;
 
     private ArrayList <Integer> tier1Rooms;
+    private ArrayList <Integer> tier2Rooms;
+    private ArrayList <Integer> tier3Rooms;
+    private ArrayList <Integer> tier4Rooms;
+    private ArrayList <Integer> tier5Rooms;
+
 
 
     public Database(){
         MainDb = new ArrayList<User>();
         SecondaryDb = new ArrayList<User>();
         tier1Rooms = new ArrayList<Integer>();
+        tier2Rooms = new ArrayList<Integer>();
+        tier3Rooms = new ArrayList<Integer>();
+        tier4Rooms = new ArrayList<Integer>();
+        tier5Rooms = new ArrayList<Integer>();
+
         populateList();
     }
 
@@ -52,19 +62,19 @@ public class Database{
         }
         if(u.getTier() == 2){
             // Case 4: No rooms have been assigned.
-            if(tier1Rooms.isEmpty()){
+            if(tier2Rooms.isEmpty()){
                 u.setRoomNum(200);
-                tier1Rooms.add(200);
+                tier2Rooms.add(200);
             }
             else{
                 // Case 5: Rooms have been assigned already, not fully booked with rooms.
-                if(tier1Rooms.size() <= 100 && tier1Rooms.get(tier1Rooms.size()-1) != 299) {
+                if(tier2Rooms.size() <= 100 && tier2Rooms.get(tier2Rooms.size()-1) != 299) {
                     int count = 0;
-                    while (tier1Rooms.size() > count) {
+                    while (tier2Rooms.size() > count) {
                         count++;
                     }
                     u.setRoomNum(count + 200);
-                    tier1Rooms.add(count + 200);
+                    tier2Rooms.add(count + 200);
                 }
                 // Case 6: Rooms have been assigned already and fully booked.
                 else{
@@ -75,19 +85,19 @@ public class Database{
         }
         if(u.getTier() == 3){
             // Case 7: No rooms have been assigned.
-            if(tier1Rooms.isEmpty()){
+            if(tier3Rooms.isEmpty()){
                 u.setRoomNum(300);
-                tier1Rooms.add(300);
+                tier3Rooms.add(300);
             }
             else{
                 // Case 8: Rooms have been assigned already, not fully booked with rooms.
-                if(tier1Rooms.size() <= 100 && tier1Rooms.get(tier1Rooms.size()-1) != 399) {
+                if(tier3Rooms.size() <= 100 && tier3Rooms.get(tier3Rooms.size()-1) != 399) {
                     int count = 0;
-                    while (tier1Rooms.size() > count) {
+                    while (tier3Rooms.size() > count) {
                         count++;
                     }
                     u.setRoomNum(count + 300);
-                    tier1Rooms.add(count + 300);
+                    tier3Rooms.add(count + 300);
                 }
                 // Case 9: Rooms have been assigned already and fully booked.
                 else{
@@ -98,19 +108,19 @@ public class Database{
         }
         if(u.getTier() == 4){
             // Case 10: No rooms have been assigned.
-            if(tier1Rooms.isEmpty()){
+            if(tier4Rooms.isEmpty()){
                 u.setRoomNum(400);
-                tier1Rooms.add(400);
+                tier4Rooms.add(400);
             }
             else{
                 // Case 11: Rooms have been assigned already, not fully booked with rooms.
-                if(tier1Rooms.size() <= 100 && tier1Rooms.get(tier1Rooms.size()-1) != 499) {
+                if(tier4Rooms.size() <= 100 && tier4Rooms.get(tier4Rooms.size()-1) != 499) {
                     int count = 0;
-                    while (tier1Rooms.size() > count) {
+                    while (tier4Rooms.size() > count) {
                         count++;
                     }
                     u.setRoomNum(count + 400);
-                    tier1Rooms.add(count + 400);
+                    tier4Rooms.add(count + 400);
                 }
                 // Case 12: Rooms have been assigned already and fully booked.
                 else{
@@ -121,19 +131,19 @@ public class Database{
         }
         if(u.getTier() == 5){
             // Case 13: No rooms have been assigned.
-            if(tier1Rooms.isEmpty()){
+            if(tier5Rooms.isEmpty()){
                 u.setRoomNum(500);
-                tier1Rooms.add(500);
+                tier5Rooms.add(500);
             }
             else{
                 // Case 14: Rooms have been assigned already, not fully booked with rooms.
-                if(tier1Rooms.size() <= 100 && tier1Rooms.get(tier1Rooms.size()-1) != 509) {
+                if(tier5Rooms.size() <= 100 && tier5Rooms.get(tier5Rooms.size()-1) != 599) {
                     int count = 0;
-                    while (tier1Rooms.size() > count) {
+                    while (tier5Rooms.size() > count) {
                         count++;
                     }
                     u.setRoomNum(count + 500);
-                    tier1Rooms.add(count + 500);
+                    tier5Rooms.add(count + 500);
                 }
                 // Case 15: Rooms have been assigned already and fully booked.
                 else{
