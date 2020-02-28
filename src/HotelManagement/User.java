70e59d0dd46e32661eeaee665714c323b341030c
Steven Villarreal <villarst@mail.gvsu.edu>
2020-02-27 23:37:27 -0500

    This contains the random password generator and the 1st release Checklist_for_1st_Release.txt

--- src/HotelManagement/User.java
+++ src/HotelManagement/User.java
@@ -133,22 +133,6 @@ public class User {
         this.dob = dob;
     }
 
-//    public String generatePassword() {
-//        int leftLimit = 48; // numeral '0'
-//        int rightLimit = 122; // letter 'z'
-//        int targetStringLength = 10;
-//        Random random = new Random();
-//
-//        String generatedString = random.ints(leftLimit, rightLimit + 1)
-//                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-//                .limit(targetStringLength)
-//                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-//                .toString();
-//
-//        System.out.println(generatedString);
-//        return generatedString;
-//    }
-
     public String generatePassWApache() {
         String generatedString = RandomStringUtils.randomAlphanumeric(10);
         return generatedString;
